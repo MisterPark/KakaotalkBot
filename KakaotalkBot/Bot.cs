6b2cfc17a4b5af12a31559cab64ab3b88816dd5a
MisterPark <40124101+MisterPark@users.noreply.github.com>
2026-06-09 21:36:36 +0900

    Update Bot.cs

--- KakaotalkBot/Bot.cs
+++ KakaotalkBot/Bot.cs
@@ -323,8 +323,8 @@ namespace KakaotalkBot
                 {
                     SmartString.CurrentNickname = command.Nickname;
                     string parsedAnswer = SmartString.Parse(answer);
-                    WindowsMacro.Instance.SetTextToChatroom(TargetWindow, $"@{command.Nickname}");
-                    //WindowsMacro.Instance.SendTextToChatroom(TargetWindow, parsedAnswer);
+                    //WindowsMacro.Instance.SetTextToChatroom(TargetWindow, $"@{command.Nickname}");
+                    WindowsMacro.Instance.SendTextToChatroom(TargetWindow, parsedAnswer);
                 }
             }
             else if (command.Keyword == "/퇴장")
